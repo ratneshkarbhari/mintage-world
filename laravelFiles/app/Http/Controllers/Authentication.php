a0a995a662aea7fa6524ead3be8499a00a803a5a
NandkumarArekar <arekars@gmail.com>
2023-08-07 17:40:16 +0530

    login page design done

--- laravelFiles/app/Http/Controllers/Authentication.php
+++ laravelFiles/app/Http/Controllers/Authentication.php
@@ -7,46 +7,43 @@ use Illuminate\Http\Request;
 
 class Authentication extends Controller
 {
-    
+
     private $salt = 'DYhG93b0qyJfIxfs2guVoUubWwvni';
 
-    function member_login(Request $request){
+    function member_login(Request $request)
+    {
 
 
         $memberModel = new Member();
 
-        $memberData = $memberModel->where("email",$request->username)->first();
+        $memberData = $memberModel->where("email", $request->username)->first();
 
         if ($memberData) {
 
-            $encryptedPassword = md5($this->salt.$request->password);
+            $encryptedPassword = md5($this->salt . $request->password);
 
 
-            if ($memberData["password"]==$encryptedPassword) {
-                $nameArray = explode(" ",$memberData["name"]);
+            if ($memberData["password"] == $encryptedPassword) {
+                $nameArray = explode(" ", $memberData["name"]);
                 session([
                     "member_id" => $memberData["id"],
                     "first_name" => $nameArray[0],
                     "last_name" => $nameArray[1],
                     "email" => $memberData["email"],
-                    "type"=>"member"
+                    "type" => "member"
                 ]);
                 return "login-success";
             } else {
                 return "login-failed";
             }
-            
         } else {
             return "login-failed";
         }
-        
-
-
     }
 
-    function logout(){
+    function logout()
+    {
         session()->flush();
-        return redirect(url("/"));
+        return redirect(url("/application/login"));
     }
-
 }
--- laravelFiles/resources/views/pages/login.blade.php
+++ laravelFiles/resources/views/pages/login.blade.php
@@ -8,7 +8,7 @@
                     <li class="breadcrumb-item me-2">
                         <a href="{{url("/")}}"><i class="fa fa-home"></i> Home</a>
                     </li>
-                    <li class="breadcrumb-item me-2">Page Name</li>                    
+                    <li class="breadcrumb-item me-2">Sign In</li>                    
                 </ol>
             </nav>
         </div>
@@ -18,9 +18,51 @@
     <section class="common-padding coing-list-wraper">
         <div class="container-fluid  px-lg-2 px-lg-5">
             <div class="d-flex justify-content-between">
-                <h2 class="mb-3 heading-1">Page Name</h2>
+                <h2 class="mb-3 heading-1">Sign In</h2>
             </div>
-             <p>Coming Soon...</p>
+            <div class="login-form-content ">
+                <div class="col-md-4 text-center company__info">
+                    <span class="company__logo">
+                        <h2><i class="fas fa-sign-in-alt"></i></h2>
+                    </span>
+                </div>
+                <div class="col-md-8 col-xs-12 col-sm-12 login_form "> 
+                    
+                    <form action="{{url("coin-info-filter-exe")}}" id="memberLoginForm">                        
+                        @csrf
+                        <input type="text" name="username" id="login-username" class="form__input" placeholder="Username">
+                        <input type="password" name="password" id="login-password" class="form__input mb-1" placeholder="Password">
+                        <span class="small text-end d-block w-100">
+                        <a href="{{url("member/forgotpassword/")}}"> Forgot password?</a> </span>
+                        <button type="button" class="btn" id="loginButton">Login</button>
+                    </form>
+                    <p class="text-danger text-center" id="loginError"></p>
+                    <script>
+                       $("button#loginButton").click(function (e) { 
+                            e.preventDefault();
+                            let username = $("input#login-username").val();
+                            let password = $("input#login-password").val();
+                            $.ajax({
+                                type: "POST",
+                                url: "{{url('member-login-exe')}}",
+                                data: {
+                                    "_token": "{{ csrf_token() }}",
+                                    "username" : username,
+                                    "password" : password
+                                },
+                                success: function (response) {
+                                    if (response=="login-success") {
+                                        location.reload();
+                                    }else{
+                                        $("p#loginError").html("Email or password is incorrect");
+                                    }
+                                }
+                            });
+                       });
+                    </script>
+                    <p class="mb-4 text-center">Don't have an account? <a href="{{url("member/")}}">Register Here</a></p>
+                </div>
+                </div>
         </div>
     </section>
     

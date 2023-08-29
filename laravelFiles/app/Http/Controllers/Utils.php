fd7c69a1f5f106d3226dfc306d670600f65fd5d8
NandkumarArekar <arekars@gmail.com>
2023-08-29 10:25:53 +0530

    create universal search only page

--- laravelFiles/app/Http/Controllers/Utils.php
+++ laravelFiles/app/Http/Controllers/Utils.php
@@ -12,24 +12,39 @@ use Illuminate\Support\Facades\DB;
 class Utils extends Controller
 {
 
-    public function universal_search() {
-        
-        $searchQuery = $_GET["q"];
 
-        $coinModel = new Coin();
-        $noteModel = new Note();
-        $stampModel = new Stamp();
-
-
-        $coinQuery = 'SELECT denomination.title, coin.obverse_image, coin.id FROM `coin` JOIN denomination ON denomination.id = coin.denomination_id JOIN ruler ON coin.ruler_id = ruler.id JOIN dynasty on ruler.dynasty_id = dynasty.id JOIN period ON dynasty.period_id = period.id  WHERE denomination.title LIKE "%'.$searchQuery.'%" OR coin.obverse_desc LIKE "%'.$searchQuery.'%"  OR coin.reverse_desc LIKE "%'.$searchQuery.'%" OR dynasty.title LIKE "%'.$searchQuery.'%" OR period.title LIKE "%'.$searchQuery.'%"';        
-        
-        $coins = DB::select($coinQuery);
-
-        $coins = json_decode(json_encode($coins),TRUE);
-        
-
-        echo $noteQuery = 'SELECT note.id, note.obverse_image, denomination.title FROM `note` JOIN denomination ON denomination.id = note.denomination_id WHERE note.obverse_description LIKE "%'.$searchQuery.'%" AND denomination.title LIKE "%'.$searchQuery.'%" AND note.reverse_description LIKE "%'.$searchQuery.'%"'; 
-        
+    private function page_loader($viewName, $data)
+    {
+        echo view("components.header", $data);
+        echo view("pages." . $viewName, $data);
+        echo view("components.footer", $data);
     }
 
+    function universal_search()
+    {
+        $this->page_loader("universal_search", [
+            "title" => "Universal Search"
+        ]);
+    }
+
+
+    // public function universal_search()
+    // {
+
+    //         $searchQuery = $_GET["q"];
+
+    //         $coinModel = new Coin();
+    //         $noteModel = new Note();
+    //         $stampModel = new Stamp();
+
+
+    //         $coinQuery = 'SELECT denomination.title, coin.obverse_image, coin.id FROM `coin` JOIN denomination ON denomination.id = coin.denomination_id JOIN ruler ON coin.ruler_id = ruler.id JOIN dynasty on ruler.dynasty_id = dynasty.id JOIN period ON dynasty.period_id = period.id  WHERE denomination.title LIKE "%' . $searchQuery . '%" OR coin.obverse_desc LIKE "%' . $searchQuery . '%"  OR coin.reverse_desc LIKE "%' . $searchQuery . '%" OR dynasty.title LIKE "%' . $searchQuery . '%" OR period.title LIKE "%' . $searchQuery . '%"';
+
+    //         $coins = DB::select($coinQuery);
+
+    //         $coins = json_decode(json_encode($coins), TRUE);
+
+
+    //         echo $noteQuery = 'SELECT note.id, note.obverse_image, denomination.title FROM `note` JOIN denomination ON denomination.id = note.denomination_id WHERE note.obverse_description LIKE "%' . $searchQuery . '%" AND denomination.title LIKE "%' . $searchQuery . '%" AND note.reverse_description LIKE "%' . $searchQuery . '%"';
+    // }
 }
---
+++ laravelFiles/resources/views/pages/universal_search.blade.php
@@ -0,0 +1,89 @@
+<main class="page-content">    
+    <section class="inside-banner"><img class="w-100 img-fluid" src="{{url("assets/images/inside-banner/default-banner.jpg")}}" /></section>
+    <section class="breadcrumb-wraper">
+        <div class="container-fluid px-lg-2 px-lg-5">
+            <nav aria-label="breadcrumb" class="breadcrumb-title-box">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item me-2">
+                        <a href="{{url("/")}}"><i class="fa fa-home"></i> Home</a>
+                    </li>
+                    <li class="breadcrumb-item me-2">Search</li>                    
+                </ol>
+            </nav>
+        </div>
+    </section>
+
+    <section class="common-padding coing-list-wraper">
+        <div class="container-fluid  px-lg-2 px-lg-5">
+            <div class="d-flex justify-content-between"><h2 class="mb-3 heading-1">Advance Search</h2></div>
+
+           <div class="row">
+            <div class="col-md-3">
+                <label for="" class="d-block mb-2">Search Keywords</label>
+                <input type="text" id="TxtSearch" class="form-control">
+            </div>
+            <div class="col-md-3">
+                <label for="" class="d-block mb-2">Select Category</label>
+                <select name="product_type" class="form-control" required="required">
+                    <option value="5" selected="selected">Shopping</option>
+                    <option value="1">Coin info</option>
+                    <option value="2">Note info</option>
+                    <option value="3">Stamp info</option>
+                    <option value="4">News</option></select>
+            </div>
+            <div class="col-md-4">
+                <label for="" class="d-md-block d-none mb-2">&nbsp;</label>
+                <button id="BtnSubmit" type="submit" class="btn btn-sm btn-explore">
+                    <i class="fa fa-fw fa-search"></i> Submit
+                    <span class="first"></span>
+                    <span class="second"></span>
+                    <span class="third"></span>
+                    <span class="fourth"></span>
+                </button>
+            </div>
+            <div class="col-md-12 mt-5">
+                <div class="search-wrap">
+                    <div class="pagination-container mb-3">
+                        <p > TOTAL 15601 RESULTS FOUND</p>
+                        <div class="result-per-page d-flex align-items-center">
+                            <p class="me-2 mb-0">Results per page</p>
+                            <select id="ddlPageSize" class="TextBox form-control" style="width: 100px">
+                                <option selected="selected">15</option>
+                                <option>25</option>
+                                <option>50</option>
+                                <option>100</option>
+                                <option>200</option>
+                            </select>
+                        </div>
+
+                    </div>
+                    <div class="pagination-container">
+                        <p>Showing 1 to 12 of 118 entries</p>
+                        <nav>
+                            <ul class="pagination">            
+                                <li class="page-item disabled" aria-disabled="true" aria-label="« Previous"><span class="page-link" aria-hidden="true">‹</span></li>
+                                <li class="page-item active" aria-current="page"><span class="page-link">1</span></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">4</a></li>
+                                <li class="page-item"><a class="page-link" href="#">5</a></li>
+                                <li class="page-item"><a class="page-link" href="#">6</a></li>
+                                <li class="page-item"><a class="page-link" href="#">7</a></li>
+                                <li class="page-item"><a class="page-link" href="#">8</a></li>
+                                <li class="page-item"><a class="page-link" href="#">9</a></li>
+                                <li class="page-item"><a class="page-link" href="#">10</a></li>
+                                <li class="page-item"><a class="page-link" href="#" rel="next" aria-label="Next »">›</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                </div>
+            </div>
+
+
+           </div>
+        </div>
+    </div>       
+  
+</main>
+ 

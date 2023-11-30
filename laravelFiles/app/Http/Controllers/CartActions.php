b8f9207ac6ffe3a6a46a209846383e69252adb8c
ratnesh-ultraindia <ratnesh.karbhari@ultraindia.com>
2023-11-30 11:42:57 +0530

    check gc availability done

--- laravelFiles/app/Http/Controllers/CartActions.php
+++ laravelFiles/app/Http/Controllers/CartActions.php
@@ -18,244 +18,211 @@ class CartActions extends Controller
         echo view("components.footer", $data);
     }
 
-    function add_to_cart_exe(Request $request){
+    function add_to_cart_exe(Request $request)
+    {
 
         $product = Product::find($request->pid);
-        if(isset(session("cart")[$product["id"]])){
+        if (isset(session("cart")[$product["id"]])) {
 
-            if($product["instock"]>=(session("cart")[$product["id"]]["quantity"]+$request->quantity)){
+            if ($product["instock"] >= (session("cart")[$product["id"]]["quantity"] + $request->quantity)) {
 
-                if(session("member_id")){
-    
+                if (session("member_id")) {
+
                     $cartObj = session("cart");
-        
-                    if(isset($cartObj[$request->pid]["quantity"])){
-    
-                        $qty = $cartObj[$request->pid]["quantity"]+$request->quantity;
-    
+
+                    if (isset($cartObj[$request->pid]["quantity"])) {
+
+                        $qty = $cartObj[$request->pid]["quantity"] + $request->quantity;
+
                         $cartObj[$request->pid] = [
                             "member_id" => session("member_id"),
                             "product" => $product,
                             "quantity" => $qty,
-                            "amount" => $product["price"]*$qty,
-                            "date_added" => date("d/m/y")
+                            "amount" => $product["price"] * $qty,
+                            "date_added" => date("d/m/y"),
+                            "card_date" => $request->date
                         ];
-    
-                    }else{
-    
+                    } else {
+
                         $cartObj[$request->pid] = [
                             "member_id" => session("member_id"),
                             "product" => $product,
                             "quantity" => $request->quantity,
-                            "amount" => $product["price"]*$request->quantity,
-                            "date_added" => date("d/m/y")
+                            "amount" => $product["price"] * $request->quantity,
+                            "date_added" => date("d/m/y"),
+                            "card_date" => $request->date
                         ];
-                        
                     }
-    
-                }else{
-        
+                } else {
+
                     $cartObj = session("cart");
-        
-                    if(isset($cartObj[$request->pid]["quantity"])){
-    
-                        $qty = $cartObj[$request->pid]["quantity"]+$request->quantity;
-    
+
+                    if (isset($cartObj[$request->pid]["quantity"])) {
+
+                        $qty = $cartObj[$request->pid]["quantity"] + $request->quantity;
+
                         $cartObj[$request->pid] = [
                             "member_id" => "NA",
                             "product" => $product,
                             "quantity" => $qty,
-                            "amount" => $product["price"]*$qty,
-                            "date_added" => date("d/m/y")
+                            "amount" => $product["price"] * $qty,
+                            "date_added" => date("d/m/y"),
+                            "card_date" => $request->date
                         ];
-                
-        
-    
-                    }else{
-    
+                    } else {
+
                         $cartObj[$request->pid] = [
                             "member_id" => "NA",
                             "product" => $product,
                             "quantity" => $request->quantity,
-                            "amount" => $product["price"]*$request->quantity,
-                            "date_added" => date("d/m/y")
+                            "amount" => $product["price"] * $request->quantity,
+                            "date_added" => date("d/m/y"),
+                            "card_date" => $request->date
                         ];
-                
-        
-    
                     }
-    
-        
                 }
-        
-                
+
+
                 session([
                     "cart" => $cartObj
                 ]);
-    
+
                 return "added-to-cart";
-    
-            }else{
-    
+            } else {
+
                 return "out-of-stock";
-    
             }
+        } else {
 
-        }else{
+            if ($product["instock"] >= $request->quantity) {
 
-            if($product["instock"]>=$request->quantity){
+                if (session("member_id")) {
 
-                if(session("member_id")){
-    
                     $cartObj = session("cart");
-        
-                    if(isset($cartObj[$request->pid]["quantity"])){
-    
-                        $qty = $cartObj[$request->pid]["quantity"]+$request->quantity;
-    
+
+                    if (isset($cartObj[$request->pid]["quantity"])) {
+
+                        $qty = $cartObj[$request->pid]["quantity"] + $request->quantity;
+
                         $cartObj[$request->pid] = [
                             "member_id" => session("member_id"),
                             "product" => $product,
                             "quantity" => $qty,
-                            "amount" => $product["price"]*$qty,
-                            "date_added" => date("d/m/y")
+                            "amount" => $product["price"] * $qty,
+                            "date_added" => date("d/m/y"),
+                            "card_date" => $request->date
                         ];
-    
-                    }else{
-    
+                    } else {
+
                         $cartObj[$request->pid] = [
                             "member_id" => session("member_id"),
                             "product" => $product,
                             "quantity" => $request->quantity,
-                            "amount" => $product["price"]*$request->quantity,
-                            "date_added" => date("d/m/y")
+                            "amount" => $product["price"] * $request->quantity,
+                            "date_added" => date("d/m/y"),
+                            "card_date" => $request->date
                         ];
-                        
                     }
-    
-                }else{
-        
+                } else {
+
                     $cartObj = session("cart");
-        
-                    if(isset($cartObj[$request->pid]["quantity"])){
-    
-                        $qty = $cartObj[$request->pid]["quantity"]+$request->quantity;
-    
+
+                    if (isset($cartObj[$request->pid]["quantity"])) {
+
+                        $qty = $cartObj[$request->pid]["quantity"] + $request->quantity;
+
                         $cartObj[$request->pid] = [
                             "member_id" => "NA",
-                            "product"=> $product,
+                            "product" => $product,
                             "quantity" => $qty,
-                            "amount" => $product["price"]*$qty,
-                            "date_added" => date("d/m/y")
+                            "amount" => $product["price"] * $qty,
+                            "date_added" => date("d/m/y"),
+                            "card_date" => $request->date
                         ];
-                
-        
-    
-                    }else{
-    
+                    } else {
+
                         $cartObj[$request->pid] = [
                             "member_id" => "NA",
                             "product" => $product,
                             "quantity" => $request->quantity,
-                            "amount" => $product["price"]*$request->quantity,
-                            "date_added" => date("d/m/y")
+                            "amount" => $product["price"] * $request->quantity,
+                            "date_added" => date("d/m/y"),
+                            "card_date" => $request->date
                         ];
-                
-        
-    
                     }
-    
-        
                 }
-        
-                
+
+
                 session([
                     "cart" => $cartObj
                 ]);
-    
-                return "added-to-cart";
-    
-            }else{
-    
-                return "out-of-stock";
-    
-            }
 
+                return "added-to-cart";
+            } else {
+
+                return "out-of-stock";
+            }
         }
-
-
-
-
     }
 
     function cart_page()
     {
 
-        $cart_items = session("cart");  
+        $cart_items = session("cart");
 
-        if(empty($cart_items)||(!isset($cart_items))){
+        if (empty($cart_items) || (!isset($cart_items))) {
             return redirect(url("shop"));
-        }else{
+        } else {
 
-            if(session("cart")){
+            if (session("cart")) {
 
                 $cartItems = session("cart");
-    
-            }else{
+            } else {
                 $cartItems = [];
             }
             $discountSet = session("discount");
-            if(isset($discountSet)){
+            if (isset($discountSet)) {
                 $discount = session("discount");
-            }else{
+            } else {
                 $discount = 0.00;
-                session(["discount"=>0.00]);
+                session(["discount" => 0.00]);
             }
-    
-    
+
+
             $this->page_loader("list_of_cart", [
                 "title" => "Cart ",
                 "cart_items" => $cartItems,
                 "discount" => $discount
             ]);
         }
-
-
     }
 
-    function increase_cart_item(Request $request){
+    function increase_cart_item(Request $request)
+    {
 
 
-        
+
         $product = Product::find($request->pid);
 
 
         $cartItems = session("cart");
 
-        $finalQty= $cartItems[$request->pid]["quantity"] =$cartItems[$request->pid]["quantity"] + 1;
+        $finalQty = $cartItems[$request->pid]["quantity"] = $cartItems[$request->pid]["quantity"] + 1;
 
-        if($product["instock"]>=$finalQty){
+        if ($product["instock"] >= $finalQty) {
 
-            if (session(["cart"=>$cartItems])) {
+            if (session(["cart" => $cartItems])) {
 
                 return 'increased-from-cart';
-                
             } else {
 
                 return 'not-increased-from-cart';
-                
             }
-
         }
-
-
-        
-
-        
-        
     }
 
-    function decrease_cart_item(Request $request){
+    function decrease_cart_item(Request $request)
+    {
 
 
 
@@ -264,27 +231,22 @@ class CartActions extends Controller
 
         $cartItems = session("cart");
 
-        $finalQty= $cartItems[$request->pid]["quantity"] = $cartItems[$request->pid]["quantity"] - 1;
+        $finalQty = $cartItems[$request->pid]["quantity"] = $cartItems[$request->pid]["quantity"] - 1;
 
-        if($product["instock"]>=$finalQty){
+        if ($product["instock"] >= $finalQty) {
 
-            if (session(["cart"=>$cartItems])) {
+            if (session(["cart" => $cartItems])) {
                 return 'decreased-from-cart';
             } else {
 
                 return 'not-decreased-from-cart';
-                
             }
-
         }
-        
-
-        
-        
     }
 
 
-    function delete_cart_item(Request $request){
+    function delete_cart_item(Request $request)
+    {
 
         $pid = $request->pid;
 
@@ -292,13 +254,11 @@ class CartActions extends Controller
 
         unset($cartItems[$pid]);
 
-        if (session(["cart"=>$cartItems])) {
+        if (session(["cart" => $cartItems])) {
             return TRUE;
         } else {
             return FALSE;
         }
-
-
     }
 
     function checkout()
@@ -306,20 +266,19 @@ class CartActions extends Controller
 
         $cart_items = session("cart");
 
-        if(empty($cart_items)||(!isset($cart_items))){
-            
+        if (empty($cart_items) || (!isset($cart_items))) {
+
             return redirect(url("shop"));
+        } else {
 
-        }else{
-
-            if (session("subtotal")<499) {
+            if (session("subtotal") < 499) {
                 $shipping = 100;
             } else {
                 $shipping = 0;
             }
-            
 
-            $payable = session("subtotal")-session('discount')+$shipping;
+
+            $payable = session("subtotal") - session('discount') + $shipping;
 
             $payable = 1.00;
 
@@ -327,9 +286,9 @@ class CartActions extends Controller
 
             $api = new Api(getenv("RAZOR_KEY"), getenv("RAZOR_SECRET"));
 
-            $order = $api->order->create(array('receipt' => uniqid(), 'amount' => $payable*100, 'currency' => 'INR'));
-    
-    
+            $order = $api->order->create(array('receipt' => uniqid(), 'amount' => $payable * 100, 'currency' => 'INR'));
+
+
 
             $this->page_loader("Checkout", [
                 "title" => "Checkout ",
@@ -338,10 +297,7 @@ class CartActions extends Controller
                 "payable" => $payable,
                 "order" => $order
             ]);
-
         }
-
-
     }
     function payment()
     {
@@ -350,16 +306,15 @@ class CartActions extends Controller
         ]);
     }
 
-    function recalculate_subtotal(){
+    function recalculate_subtotal()
+    {
         $subtotal = 0.00;
 
 
-        foreach(session("cart") as $cartItem){
+        foreach (session("cart") as $cartItem) {
 
-            $subtotal+=$cartItem["amount"];
-
+            $subtotal += $cartItem["amount"];
         }
         return $subtotal;
     }
-
 }
--- laravelFiles/app/Http/Controllers/Shopping.php
+++ laravelFiles/app/Http/Controllers/Shopping.php
@@ -19,46 +19,42 @@ class Shopping extends Controller
         echo view("components.footer", $data);
     }
 
-    private function fetch_random_products($catIds){
+    private function fetch_random_products($catIds)
+    {
 
-        return json_decode(json_encode(DB::table('products')->where('category', $catIds)->where("status","Active")->where("instock",">",0)->where("featured",1)->limit(7)->paginate(12)),TRUE);
-
+        return json_decode(json_encode(DB::table('products')->where('category', $catIds)->where("status", "Active")->where("instock", ">", 0)->where("featured", 1)->limit(7)->paginate(12)), TRUE);
     }
 
     function shop()
     {
 
         if (!$featuredCoins = Cache::get("featured_coins")) {
-            
+
             $coinsQuery = 'SELECT * FROM products WHERE featured = 1 AND status = "Active" AND instock != 0 AND category IN (18,24,25,26,27,28) LIMIT 7';
 
-            $featuredCoins = json_decode(json_encode(DB::select($coinsQuery)),TRUE);
+            $featuredCoins = json_decode(json_encode(DB::select($coinsQuery)), TRUE);
 
-            Cache::set("featured_coins",$featuredCoins);
-            
+            Cache::set("featured_coins", $featuredCoins);
         }
 
         if (!$featuredNotes = Cache::get("featured_notes")) {
-            
+
             $notesQuery = 'SELECT * FROM products WHERE featured = 1 AND status = "Active" AND instock != 0 AND category IN (35,36,37,38,39,40,41,42,43) LIMIT 7';
 
-            $featuredNotes = json_decode(json_encode(DB::select($notesQuery)),TRUE);
+            $featuredNotes = json_decode(json_encode(DB::select($notesQuery)), TRUE);
 
-            Cache::set("featured_notes",$featuredNotes);
-            
+            Cache::set("featured_notes", $featuredNotes);
         }
-        
+
 
         if (!$featuredAccessories = Cache::get("featured_accessories")) {
 
             $accessoryQuery = 'SELECT * FROM products WHERE featured = 1 AND status = "Active" AND instock != 0 AND category IN (11,12,13,14) LIMIT 7';
 
-            $featuredAccessories = json_decode(json_encode(DB::select($accessoryQuery)),TRUE);
+            $featuredAccessories = json_decode(json_encode(DB::select($accessoryQuery)), TRUE);
 
 
-            Cache::set("featured_accessories",$featuredNotes);
-
-
+            Cache::set("featured_accessories", $featuredNotes);
         }
 
 
@@ -67,10 +63,9 @@ class Shopping extends Controller
 
             $giftCardQuery = 'SELECT * FROM products WHERE featured = 1 AND status = "Active" AND instock != 0 AND category IN (6) LIMIT 7';
 
-            $featuredGiftCards = json_decode(json_encode(DB::select($giftCardQuery)),TRUE);
+            $featuredGiftCards = json_decode(json_encode(DB::select($giftCardQuery)), TRUE);
 
-            Cache::get("featured_gift_cards",$featuredGiftCards);
-
+            Cache::get("featured_gift_cards", $featuredGiftCards);
         }
 
 
@@ -82,71 +77,67 @@ class Shopping extends Controller
             "random_greeting_cards" => $featuredGiftCards
         ]);
     }
-    function shop_list($categorySlug,Request $request)
+    function shop_list($categorySlug, Request $request)
     {
 
 
-        
-        $categorySlugParts = explode("-",$categorySlug);
+
+        $categorySlugParts = explode("-", $categorySlug);
 
         $categoryProducts = Product::select('*');
 
 
-        $categoryProducts = $categoryProducts->where("instock","!=",0);
+        $categoryProducts = $categoryProducts->where("instock", "!=", 0);
 
 
-        $categoryProducts = $categoryProducts->where("category",$categorySlugParts[0]);
+        $categoryProducts = $categoryProducts->where("category", $categorySlugParts[0]);
 
-        if ($request->has('price_sort')&&$request->price_sort!="") {
+        if ($request->has('price_sort') && $request->price_sort != "") {
 
-            $categoryProducts = $categoryProducts->orderBy("price",$request->price_sort);
-
+            $categoryProducts = $categoryProducts->orderBy("price", $request->price_sort);
         }
 
-        if($request->has("denomination")&&$request->denomination!=""){
+        if ($request->has("denomination") && $request->denomination != "") {
 
-            $categoryProducts = $categoryProducts->where("denomination",$request->denomination);
-
+            $categoryProducts = $categoryProducts->where("denomination", $request->denomination);
         }
 
 
-        
-        if($request->has("governor")&&$request->governor!=""){
 
-            $categoryProducts = $categoryProducts->where("governor",$request->governor);
+        if ($request->has("governor") && $request->governor != "") {
 
+            $categoryProducts = $categoryProducts->where("governor", $request->governor);
         }
 
-        $categoryProducts  = $categoryProducts->paginate(12);  
-        
+        $categoryProducts  = $categoryProducts->paginate(12);
+
         $maincatdata = ProductCategory::find($categorySlugParts[0]);
 
-        if($maincatdata["parent"]!=0){
-            $parent_category = ProductCategory::find($maincatdata["parent"]);            
-            if($parent_category["parent"]!=0){
-                $grand_parent_category = ProductCategory::find($parent_category["parent"]);            
-            }else{
+        if ($maincatdata["parent"] != 0) {
+            $parent_category = ProductCategory::find($maincatdata["parent"]);
+            if ($parent_category["parent"] != 0) {
+                $grand_parent_category = ProductCategory::find($parent_category["parent"]);
+            } else {
                 $grand_parent_category = NULL;
             }
-        }else{
+        } else {
             $parent_category = NULL;
         }
 
 
         $total = count($categoryProducts);
 
-        if ($total==0) {
+        if ($total == 0) {
 
             $productCategory = new ProductCategory();
 
-            $childCategories = $productCategory->where("parent",$categorySlugParts[0])->paginate(12);
+            $childCategories = $productCategory->where("parent", $categorySlugParts[0])->paginate(12);
 
             $childCatIds = [];
 
-            foreach($childCategories as $childCategory){
+            foreach ($childCategories as $childCategory) {
 
                 $childCatIds[] = $childCategory["id"];
-
             }
 
 
@@ -154,38 +145,33 @@ class Shopping extends Controller
             $categoryProducts = Product::select('*');
 
 
-            $categoryProducts = $categoryProducts->where("category",$childCatIds);
+            $categoryProducts = $categoryProducts->where("category", $childCatIds);
 
-            $categoryProducts = $categoryProducts->where("instock","!=",0);
+            $categoryProducts = $categoryProducts->where("instock", "!=", 0);
 
 
-            if ($request->has('price_sort')&&$request->price_sort!="") {
-    
-                $categoryProducts = $categoryProducts->orderBy("price",$request->price_sort);
-    
+            if ($request->has('price_sort') && $request->price_sort != "") {
+
+                $categoryProducts = $categoryProducts->orderBy("price", $request->price_sort);
             }
-    
-            if($request->has("denomination")&&$request->denomination!=""){
-    
-                $categoryProducts = $categoryProducts->where("denomination",$request->denomination);
-    
+
+            if ($request->has("denomination") && $request->denomination != "") {
+
+                $categoryProducts = $categoryProducts->where("denomination", $request->denomination);
             }
-    
-    
-            
-            if($request->has("governor")&&$request->governor!=""){
-    
-                $categoryProducts = $categoryProducts->where("governor",$request->governor);
-    
+
+
+
+            if ($request->has("governor") && $request->governor != "") {
+
+                $categoryProducts = $categoryProducts->where("governor", $request->governor);
             }
-    
-            $categoryProducts  = $categoryProducts->paginate(12);  
-    
+
+            $categoryProducts  = $categoryProducts->paginate(12);
+
 
 
             $total = count($categoryProducts);
-
-
         }
 
         $currentPage = $categoryProducts->currentPage();
@@ -196,12 +182,12 @@ class Shopping extends Controller
 
         $paginationInfoString = "Showing {$from} to {$to} of {$total} entries";
 
-        if(!isset($grand_parent_category)){
+        if (!isset($grand_parent_category)) {
             $grand_parent_category = NULL;
         }
-        
+
         $this->page_loader("shop_list", [
-            "title" => "Buy Coins of ". $maincatdata["cat_name"] ." Online | Mintage World",
+            "title" => "Buy Coins of " . $maincatdata["cat_name"] . " Online | Mintage World",
             "grand_parent_category" => $grand_parent_category,
             "parent_category" => $parent_category,
             "category" => $maincatdata,
@@ -214,62 +200,66 @@ class Shopping extends Controller
     {
 
 
-        $slugParts = explode("-",$productSlug);
+        $slugParts = explode("-", $productSlug);
 
-        $product = Product::where("id",$slugParts[0])->with("product_category")->with("product_images")->with("product_ratings")->first();
+        $product = Product::where("id", $slugParts[0])->with("product_category")->with("product_images")->with("product_ratings")->first();
 
 
 
-        if($product){
+        if ($product) {
 
             $this->page_loader("view_product", [
-                "title" => "Buy ".$product["name1"]." Online",
+                "title" => "Buy " . $product["name1"] . " Online",
                 "product" => $product,
                 "category" => $product["product_category"]
             ]);
-
-        }else{
+        } else {
 
             return redirect()->to(url("shop"));
-
         }
-
-
     }
 
-    function check_note_availability (Request $request){
+    function check_note_availability(Request $request)
+    {
+
+
 
         $productSpecial = new ProductSpecial();
 
-        $productId = $request->pid;
+        $productId = $request->product_id;
         $quantity = $request->quantity;
         $catId = $request->cat_id;
         $price = $request->price;
-        $date = $request->date;
+        $date = $request->date."-".$request->month."-".$request->year;
 
 
-        if ($productSpecial->where("product_id",$productId)->where("date_start",$date)->where("status","Yes")) {
-            
+        $res = $productSpecial->where("product_id",$productId)->where("date_start",$date)->where("status","Yes")->first();
+
+
+        if ($res) {
+
             return json_encode([
                 "message" => "Available at premium price"
             ]);
+        } else {
+
             
+
+            if ($res = $productSpecial->where("product_id", $productId)->where("date_start", $date)->where("status", "No")->first()) {
+
+                return json_encode([
+                    "message" => "Not Available"
+                ]);
+            } else {
+
+                return json_encode([
+                    "message" => "Product Available"
+                ]);
+            }
         }
 
-        if ($productSpecial->where("product_id",$productId)->where("date_start",$date)->where("status","No")) {
-            return json_encode([
-                "message" => "Not Available"
-            ]);
-        }
 
-        if (!$productSpecial->where("product_id",$productId)->where("date_start",$date)) {
 
-            return json_encode([
-                "message" => "Product Available"
-            ]);
-
-        }
 
     }
-
 }

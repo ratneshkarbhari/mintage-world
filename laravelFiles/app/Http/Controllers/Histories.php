2f38cd7b94a6b1eeef482e223ace48c3b51abcdf
ratnesh-ultraindia <ratnesh.karbhari@ultraindia.com>
2024-03-20 15:01:24 +0530

    fixed history bug

--- laravelFiles/app/Http/Controllers/Histories.php
+++ laravelFiles/app/Http/Controllers/Histories.php
@@ -114,9 +114,11 @@ class Histories extends Controller
 
     function get_histories_grid_for_period(){
 
-        $period_id = $_GET["period_id"];
+        $period_id = str_replace("/","",$_GET["period_id"]);
 
+        
         $histories = DB::select('SELECT history.id,dynasty.title, dynasty.image FROM dynasty JOIN history ON dynasty.id = history.dynasty_id JOIN period ON period.id = dynasty.period_id WHERE period.category_id = 1 AND period.country_id = 1 AND period.id='.$period_id.' AND history.history!="" AND history.download_file!=""');
+
 
 
         $histories = json_decode(json_encode($histories),TRUE);
